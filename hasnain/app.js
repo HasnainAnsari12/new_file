41e7a80719b13ccefb14580b6934004d10700c12
HasnainAnsari12 <121307333+HasnainAnsari12@users.noreply.github.com>
2023-02-22 11:07:12 +0500

    Add files via upload

---
+++ hasnain/app.js
@@ -0,0 +1,14 @@
+// var hasnain=["mango","pineapple","leeche","banana"];
+
+// for(var i=0; i <hasnain.length ;i++);{
+//     console.log(i);
+
+
+
+
+var stdArr = ["Ali", "john", "bilal", "sufiyan", "Jaffar", "Aman", "talha"]
+
+for(var i=0 ; i < stdArr.length ; i++){
+    console.log(stdArr[i]);
+}
+
